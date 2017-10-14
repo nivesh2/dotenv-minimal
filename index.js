03ea94c203e7561bd3950ffb1245536c2bf47f6d
Nivesh <nivesh@admins-MacBook-Pro.local>
2017-10-14 19:32:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * Loads environment variables from .env file
+ * check for the missing environment variables at start
+ * throw exception if .env file doesnot exist
+ * @returns env
+ */
+function readFromEnvFile() {
+  try {
+    const data = require('fs').readFileSync('.env', 'utf8')
+
+    const env = {}
+    data.split('\n').filter((v) => {
+      return v !== ''
+    }).filter((v) => {
+      return v.indexOf('#') === -1
+    }).forEach((v) => {
+      const temp = v.split('=').map((str) => {
+        return str.trim()
+      })
+      env[temp[0]] = temp[1]
+    })
+    return env
+  } catch (err) {
+    throw err
+  }
+}
+
+// it only supports spilitting using comma
+function splitMultipleValues(envObject) {
+  try {
+    const data = Object.keys(envObject)
+    return data.reduce((p, v) => {
+      p[v] = envObject[v]
+      if (envObject[v].indexOf(',') !== -1) {
+        p[v] = envObject[v].split(',')
+      }
+      return p
+    }, {})
+  } catch (err) {
+    throw err
+  }
+}
+
+module.exports = (() => {
+  try {
+    const temp = splitMultipleValues(readFromEnvFile())
+    process.env = temp
+    return temp
+  } catch (err) {
+    throw err
+  }
+})()
